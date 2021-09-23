a45eb17dbf2a25e208bebfe939ae5918023eeeb2
prajwal k <78892062+prajjudynamics@users.noreply.github.com>
2021-09-23 15:25:05 +0530

    Add files via upload
    This file consists functions used for meteorological purposses.(*beta version)

---
+++ prajjudynamics.py
@@ -0,0 +1,22 @@
+# libraries required
+import xarray as xr
+import numpy as np
+import glob
+import pandas as pd
+
+
+
+
+
+#function for calculating relative humidity using specific humidity,temperature and pressure data
+def RH(spec_hum,T,pre):
+	vp = (spec_hum*pre)/0.622 # in pascals
+	a = -6096.9385
+	b = 21.2409642
+	c = -2.711193e-2
+	d = 1.673952e-5
+	e = 2.433502
+	vs = xr.ufuncs.exp( (a/T) + b + (c*T) + (d*T**2) + e*xr.ufuncs.log(T))
+
+	RH = 100*(vp / vs)
+	return (RH)
